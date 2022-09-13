ecad85fd4cdc93fee91aa285c6f789bc393542f5
Pau Civill <pau.civill@gmail.com>
2022-09-13 09:01:55 +0200

    update

--- functions.php
+++ functions.php
@@ -65,62 +65,11 @@ function listItems($items) {
     }
 }
 
-// Subcarpetas *********************************************
+// Subcarpetas
 $urlName = $_GET["name"];
 #$folderContent = scandir("./root/$urlName"); // TENDRÍA QUE PODER REDEFINIR FOLDER CONTENT !!!!!
 $path = "./root/$urlName";
 $folderContent = scandir($path);
-
-// PRUEBASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
-
-function listSub($items, $p) {
-    foreach ($items as $item) {
-        if ($item != "." && $item != "..") {
-            ?>
-
-            <div class="folder">
-                <li><a href="index.php?name=<?php echo $p ?>"><?php echo $item ?></a></li>
-            </div>
-
-            <?php
-        }
-    }
-    // $p = $p . "/" . $_GET["name"]; 
-}
-
-# $openFolderContent = scandir("./root." . $_GET["name"]);
-
-function listSubItems($path) {
-    // if (isset($_GET["name"])) {
-        $folderName = $_GET["name"];
-
-        // if (is_dir($folderName)) {
-            $folderOpened = opendir($path);
-            while ($item = readdir($folderOpened)) {
-                $newPath = $path."/".$item;
-                if (is_dir($newPath) && $item != "." && $item != "..") {
-                    echo "Found Folder $newPath <br>";
-                    listSubItems($newPath);
-                } else {
-                    ?>
-
-                    <!-- <div class="folder">
-                        <li><a href="index.php?name=<?php echo $item ?>"><?php echo $item ?></a></li>
-                    </div> -->
-
-                    <?php
-                }
-            }
-
-            
-        // }
-    // }
-}
-
-$path = "/";
-echo "$path <br>";
-listSubItems($path);
-// PRUEBASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 
 // _____________________________________________________________________________
 
